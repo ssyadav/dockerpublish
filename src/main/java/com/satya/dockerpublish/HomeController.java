a5b68ee52deca2178d23fe5fee47f6ae0eb32d00
Satyaveer yadav <ssyadav.in@gmail.com>
2021-12-30 22:29:32 +0530

    Update HomeController.java

--- src/main/java/com/satya/dockerpublish/HomeController.java
+++ src/main/java/com/satya/dockerpublish/HomeController.java
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * File: HomeController.java Copyright 2021 THERMOFISHER SCIENTIFIC. All rights reserved.
+ * File: HomeController.java Copyright 2021. All rights reserved.
  * <p>
- * This software is the confidential and proprietary information of THERMOFISHER SCIENTIFIC.
+ * This software is the confidential and proprietary information of ABC.
  * You shall not disclose such Confidential Information and shall use it
  * only in accordance with the terms of the license agreement you entered into
- * with THERMOFISHER SCIENTIFIC.
+ * with ABC.
  * <p>
- * Created on: 24/12/21, Created by: satyaveer.yadav, Version: 1.0,  Project Name: Genexus/Ion Reporter
+ * Created on: 24/12/21, Created by: satyaveer.yadav, Version: 1.0,  Project Name: dockerpublish
  * Last updated by: satyaveer.yadav
  */
 @RestController
